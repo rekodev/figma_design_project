93452748c68d32c07ebf49036063adc5bbd07d74
zadr0tishe <112801448+zadr0tishe@users.noreply.github.com>
2023-02-12 00:22:20 +0200

    Added header menu button functionality, where the menu disappears if the user clicks anywhere on the page.

--- header-menu.js
+++ header-menu.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById('menu');
 const nav = document.getElementById('header-nav');
+const faSolid = document.querySelectorAll('.fa-solid');
 const mediaQueryDesktop = window.matchMedia('(min-width: 1280px)');
 
 function toggleNav() {
@@ -11,10 +12,22 @@ function toggleNav() {
 }
 
 menuButton.addEventListener('click', toggleNav);
+
 window.addEventListener('resize', () => {
   if (mediaQueryDesktop.matches) {
     nav.style.display = 'flex';
   } else {
+    nav.style.display = 'none';
+  }
+});
+
+// If clicked outside of menu, the menu will close
+window.addEventListener('click', (e) => {
+  if (
+    !e.target.matches('.button') &&
+    !e.target.matches('.fa-solid') &&
+    nav.style.display != 'none'
+  ) {
     nav.style.display = 'none';
   }
 });
